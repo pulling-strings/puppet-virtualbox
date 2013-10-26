dc584a4538386bdfe676ec3617fe3f325b34db42
narkisr <narkisr@gmail.com>
2013-10-26 22:26:44 +0300

    fixing url

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -1,8 +1,8 @@
 # Setting up the extension module
 class virtualbox::extension {
 
-  $tar = 'Oracle_VM_VirtualBox_Extension_Pack-4.2.6-82870.vbox-extpack'
-  $url = "http://download.virtualbox.org/virtualbox/4.2.6/${tar}"
+  $tar = 'Oracle_VM_VirtualBox_Extension_Pack-4.2.18-88780.vbox-extpack'
+  $url = "http://download.virtualbox.org/virtualbox/4.2.18/${tar}"
 
   exec{'install extension':
     command     => "wget ${url} -P /tmp && vboxmanage extpack install /tmp/${tar}",
