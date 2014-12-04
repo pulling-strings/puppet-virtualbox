74dfcbe5c97755f3fb2c925a0011d59ec27d4677
ronen <narkisr@gmail.com>
2014-12-04 17:16:27 +0200

    adding hostkey

--- manifests/manage.pp
+++ manifests/manage.pp
@@ -1,8 +1,16 @@
-# Misc managment options
+# VBoxManage options:
+#  $hostkey_comb possible values (only on Linux see http://bit.ly/1wxVco2):
+#    left_alt, right_alt, right_cont, left_cont
 class virtualbox::manage(
   $machinefolder = '',
-  $user = ''
+  $user = '',
+  $hostkey_comb = ''
 ){
+  $keys = {
+    left_alt   => '65513', right_alt => '65514',
+    right_ctrl => '65508', left_ctrl => '65507'
+  }
+
   if($machinefolder != ''){
     exec{"set machinefolder to ${machinefolder}":
       command => "vboxmanage setproperty machinefolder ${machinefolder}",
@@ -10,5 +18,16 @@ class virtualbox::manage(
       path    => ['/usr/bin','/bin',],
       require => Package[$virtualbox::package_real]
     }
+  }
+
+  if($hostkey_comb != '' and $desktop::is_desktop){
+    $key = $keys[$hostkey_comb]
+    exec{"set hostkey_comb to ${$hostkey_comb}":
+      command => "vboxmanage setextradata global GUI/Input/HostKeyCombination ${key}",
+      user    => $user,
+      path    => ['/usr/bin','/bin',],
+      require => Package[$virtualbox::package_real]
+    }
+
   }
 }
